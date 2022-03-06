8b756db6beba260e803cdb48f531297f4e5568d7
zeltapirksti <aivis.jekabsons@gmail.com>
2022-03-06 20:31:49 +0200

    next

--- uzdevumi.py
+++ uzdevumi.py
@@ -9,13 +9,24 @@
 #         j += 1
 #     print("")
 
+# number = int(input("Please input integer: "))
+# total_sum = 0
+# for i in range(1, number):
+#     total_sum += i
+#     print(i, end=' + ')
+# total_sum += number
+# print(number, end=' = ')
+# print(total_sum)
 
-number = int(input("Please input integer: "))
-total_sum = 0
-while i < number:
-for i in range(1, number + 1):
-    total_sum += i
-    print(i, end=' + ')
-print("Total sum is: ", total_sum)
+is_prime = False
+a = int(input("Please input number to check whether it's prime: "))
+for i in range(a - 1, 2, -1):
+    print(i)
+    if a % i == 0:
+        is_prime = True
+        break
+if is_prime == True:
+    print(a, "is a prime number :)")
+else:
+    print(a, "is not a prime number :(")
 
-# Test gh !!!!!!!!!!!!!!!!!!!!!!!!
