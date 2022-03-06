d9268ce88597f59892a7c239a9c595ae62fd0303
Aivis Jekabsons <jekacaiv@Aiviss-MacBook-Pro.local>
2022-03-06 19:46:04 +0200

    Initial commit

---
+++ uzdevumi.py
@@ -0,0 +1,19 @@
+# p = 0
+# while p < 10:
+#     print(p)
+#     p = p + 1
+
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#         j += 1
+#     print("")
+
+
+number = int(input("Please input integer: "))
+total_sum = 0
+while i < number:
+for i in range(1, number + 1):
+    total_sum += i
+    print(i, end=' + ')
+print("Total sum is: ", total_sum)
