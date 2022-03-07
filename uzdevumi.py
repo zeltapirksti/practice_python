e5a69d5628dd625bdfece8519843517951e6ab4b
zeltapirksti <aivis.jekabsons@gmail.com>
2022-03-07 17:21:38 +0200

    next 2

--- uzdevumi.py
+++ uzdevumi.py
@@ -9,6 +9,8 @@
 #         j += 1
 #     print("")
 
+#############################################################################
+
 # number = int(input("Please input integer: "))
 # total_sum = 0
 # for i in range(1, number):
@@ -18,15 +20,64 @@
 # print(number, end=' = ')
 # print(total_sum)
 
-is_prime = False
-a = int(input("Please input number to check whether it's prime: "))
-for i in range(a - 1, 2, -1):
-    print(i)
-    if a % i == 0:
-        is_prime = True
-        break
-if is_prime == True:
-    print(a, "is a prime number :)")
-else:
-    print(a, "is not a prime number :(")
+#############################################################################
+
+# is_prime = False
+# a = int(input("Please input number to check whether it's prime: "))
+# for i in range(a - 1, 2, -1):
+#     print(i)
+#     if a % i == 0:
+#         is_prime = True
+#         break
+# if is_prime == True:
+#     print(a, "is a prime number :)")
+# else:
+#     print(a, "is not a prime number :(")
+
+#############################################################################
+
+# n = int(input("Choose a number to create Multiplication Table: "))
+# print("Multiplication Table for", n)
+# for i in range(1, 10, 1):
+#     p = i * n
+#     print(i, "x", n, "=", p)
+
+#############################################################################
+
+# numbers = [12, 75, 150, 180, 145, 525, 50]
+# for i in numbers:
+#     if i > 500:
+#         break
+#     elif i > 150:
+#         continue
+#     elif i % 5 == 0:
+#         print(i)
+
+#############################################################################
+
+# num = int(input("Please input your number: "))
+# s_counter = 0
+# while num != 0:
+#     num = num // 10
+#     s_counter += 1
+# print(s_counter)
+
+#############################################################################
+
+# row_n = int(input("How long? "))
+# for i in range(1, row_n + 1):
+#     for j in range(row_n + 1 - i, 0, -1):
+#         print(j, end=' ')
+#     print(end='\n')
+
+#############################################################################
+
+# list1 = [10, 20, 30, 40, 50]
+# sizee = len(list1) - 1
+# for i in range(sizee, -1, -1):
+#     print(list1[i])
+
+sdfsdf
+
+
 
